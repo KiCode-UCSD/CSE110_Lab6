1857d4a984247f5593516b090eb678b00ebd90a9
KiCode-UCSD <r8diaz@ucsd.edu>
2026-05-11 16:53:57 -0700

    Finished expose and explore sections

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,7 +8,10 @@ function init() {
 	// Get the recipes from localStorage
 	let recipes = getRecipesFromStorage();
 	// Add each recipe to the <main> element
-	addRecipesToDocument(recipes);
+	if (recipes !== null) {
+		addRecipesToDocument(recipes);
+	}
+	
 	// Add the event listeners to the form elements
 	initFormHandler();
 }
@@ -24,6 +27,9 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	let arr = [];
+	arr = JSON.parse(localStorage.getItem("recipes"));
+	return arr;
 }
 
 /**
@@ -35,10 +41,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const mainRef = document.querySelector("main");
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	recipes.forEach((recipe) => {
+		const recipeCard = document.createElement("recipe-card");
+		recipeCard.data = recipe;
+		mainRef.append(recipeCard);
+	});
 }
 
 /**
@@ -51,6 +63,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem("recipes", JSON.stringify(recipes));
 }
 
 /**
@@ -59,21 +72,58 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
+	const formEl = document.querySelector("form");
+
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
+	formEl.addEventListener("submit", (event) => handleSubmit(formEl));
+
 	// Steps B4-B9 will occur inside the event listener from step B3
+	
+	// B10. TODO - Get a reference to the "Clear Local Storage" button
+	const clearLocStor = document.querySelector("button.danger");
+
+	// B11. TODO - Add a click event listener to clear local storage button
+	clearLocStor.addEventListener("click", () => {
+		// Steps B12 & B13 will occur inside the event listener from step B11
+		// B12. TODO - Clear the local storage
+		localStorage.clear();
+
+		// B13. TODO - Delete the contents of <main>
+		document.querySelector("main").innerHTML = "";
+	});
+}
+
+function handleSubmit(formEl) {
+	event.preventDefault();
+
 	// B4. TODO - Create a new FormData object from the <form> element reference above
+	const formDataObj = new FormData(formEl);
+
 	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
 	//            make this easier to read), and then extract the keys and corresponding
 	//            values from the FormData object and insert them into recipeObject
+	const recipeObject = {};
+	formDataObj.forEach((value, key) => {
+		recipeObject[key] = value;
+	});
+
 	// B6. TODO - Create a new <recipe-card> element
+	const recipeCard = document.createElement("recipe-card");
+
 	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+	recipeCard.data = recipeObject;
+
 	// B8. TODO - Append this new <recipe-card> to <main>
+	document.querySelector("main").append(recipeCard);
+
 	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
 	//            then save the recipes array back to localStorage
-	// B10. TODO - Get a reference to the "Clear Local Storage" button
-	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	// B13. TODO - Delete the contents of <main>
+	const recipes = getRecipesFromStorage();
+	recipes.push(recipeObject);
+	saveRecipesToStorage(recipes);
+
+	addRecipesToDocument(recipeObject);
+
+	form.reset();
 }
